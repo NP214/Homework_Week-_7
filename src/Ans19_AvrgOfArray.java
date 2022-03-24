33a0313d74ed22b3db216946d67a6a5b18fae810
NP214 <101005159+NP214@users.noreply.github.com>
2022-03-24 13:15:53 -0700

    Update Ans19_AvrgOfArray.java

--- src/Ans19_AvrgOfArray.java
+++ src/Ans19_AvrgOfArray.java
@@ -2,13 +2,14 @@ public class Ans19_AvrgOfArray {
     // 19.	Write a Java program to calculate the average value of array elements.
 
     public static void main(String[] args) {
-//        int a[]={10,20,30};
-//        int sum = 0;
-//        for(int i = 0; i<a.length; i++){
-//            sum = sum + a[i];
-//            double average = sum/ a.length;
-//            System.out.println("Average of the array is " + average);
+        int a[]={10,20,30};
+     int sum = 0;
+      for(int i = 0; i<a.length; i++){
+            sum = sum + a[i];
+           double average = sum/ a.length;
+          
         }
+        System.out.println("Average of the array is " + average);
     }
 
 
